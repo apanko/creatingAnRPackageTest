9a9197e100bc1ec55e03add09ead483f1d6898f3
apanko <apanko@umich.edu>
2016-12-05 12:24:38 -0500

    adding data stuff

---
+++ R/testMatrix.R
@@ -0,0 +1,11 @@
+#' a matrix of 1
+#' 
+#' a dataset containing 1's
+#' @format a matrix with 4 rows and 4 columns
+#' \desribe{
+#' \item{V1}{column1}
+#' \item{V2}{column2}
+#' \item{V3}{column3}
+#' \item{V4}{column4}
+#' }
+"testMatrix"
